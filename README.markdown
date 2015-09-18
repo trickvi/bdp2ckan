56e2ec956b37e07b8b5c925ddcdd13e909ab4ec1
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2015-09-18 17:44:14 +0000

    Initial commit of a script that turns a BDP into a CKAN dataset

---
+++ README.markdown
@@ -0,0 +1,25 @@
+# BDP2CKAN
+
+A proof of concept command line script capable of submitting a Budget Data Package to a CKAN instance. The CKAN instance must be able to store metadata extras. This should use the python [datapackage](https://pypi.python.org/pypi/datapackage/) library but doesn't because currently that library doesn't support budget data packages (but the work done for this could be used to improve the datapackage library). Anyways, onwards to the important stuff.
+
+## Installation
+
+As this is a proof of concept, this does not provide a nice python setup. You have to do manual stuff to get it working but it's not the end of the world; it's still kind of standard. Create a virtualenv (recommended) and pip install the requirements.
+
+    virtualenv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+## Usage
+
+To see how it works type:
+
+    python bdp2ckan.py --help
+
+But it's very simple. You basically just provide a schema file (we provide it for you because we're super nice), the CKAN host, your API key for that CKAN host and a link to the datapackage.json descriptor file.
+
+    python bdp2ckan.py --schema schemas/0.3.0.json --host 'http://localhost:5000' --apikey <my-awesome-api-key> https://raw.githubusercontent.com/os-data/boost-armenia/master/datapackage.json
+
+## License
+
+bdp2ckan is available under the GNU General Public License, version 3. See LICENCE for more details.
---
+++ bdp2ckan.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+# bdp2ckan.py - Send a budget data package to a CKAN instance
+# Copyright (C) 2013 Tryggvi Björgvinsson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import click
+import json
+import jsonschema
+import requests
+import urlparse
+
+
+def create_ckan_package_dict(descriptor):
+    """
+    Convert metadata from a data package to CKAN metadata
+    """
+    
+    # Mapping between metadata keys of CKAN and Data package
+    # This does not handle licenses (multiple licenses in the data package)
+    # because CKAN does not support it
+    ckan_mapping = [('name', 'name'), ('title', 'title'), ('url', 'homepage'),
+                    ('version', 'version'), ('license_id', 'license'),
+                    ('notes', 'description')]
+
+    # Extract available CKAN metadata from the data package
+    data_dict = {}
+    for (ckan, dpkg) in ckan_mapping:
+        if dpkg in descriptor:
+            data_dict[ckan] = descriptor[dpkg]
+
+    return data_dict
+
+
+def create_ckan_resource_array(descriptor):
+    """
+    Create a CKAN resource array from data package resources
+    """
+    # Mapping between resource metadata keys of CKAN and Data package
+    # name and path can be overwritten by title and url respectively
+    resource_mapping = [('name', 'name'), ('name', 'title'),
+                        ('description', 'description'), ('format', 'format'),
+                        ('mimetype', 'mediatype'), ('size', 'bytes'),
+                        ('hash', 'hash'), ('url', 'path'), ('url', 'url')]
+
+    # Extract CKAN resources and associated metadata from data package
+    resource_array = {'resources': []}
+    for resource in descriptor['resources']:
+        data_dict = {}
+            
+        for (ckan, dpkg) in resource_mapping:
+            if dpkg in resource:
+                data_dict[ckan] = resource[dpkg]
+        resource_array['resources'].append(data_dict)
+
+    return resource_array
+
+
+def create_budget_data_package_extras(descriptor):
+    """
+    Create a CKAN extras array from budget data package specific metadata
+    """
+    # Mapping between metadata keys of CKAN extras and Budget data package
+    # This requires these particular keys to be set on the CKAN instance
+    # Mapping is excluded because that's just going to look really bad in CKAN
+    bdp_mapping = [('granularity', 'granularity'), ('direction', 'direction'),
+                   ('status', 'status'), ('country', 'countryCode')]
+
+    # Extract budget data package metadata as CKAN extras metadata
+    data_dict = {'extras':[]}
+    for (ckan, dpkg) in bdp_mapping:
+        if dpkg in descriptor:
+            data_dict['extras'].append({'key': dpkg, 'value': descriptor[dpkg]})
+
+    return data_dict
+
+
+def submit_to_ckan(host, apikey, data):
+    """
+    Submit a CKAN data dictionary to a given host with a given api key
+    """
+
+    # Put together the api url and authorization headers and send the data
+    package_create_url = urlparse.urljoin(host, '/api/action/package_create')
+    headers = {'Authorization': apikey}
+    response = requests.post(package_create_url, headers=headers, json=data)
+
+    # Why create a nice response if you can make a crappy one?
+    return response.status_code
+
+
+@click.command()
+@click.option('--schema', default=None, nargs=1, type=click.File('r'),
+              help='Schema to validate against')
+@click.option('--host', default='localhost', nargs=1,
+              help='CKAN instance to upload to')
+@click.option('--apikey', default=None, nargs=1,
+              help='CKAN user API key of uploader')
+@click.argument('datapackage')
+def bdp2ckan(schema, host, apikey, datapackage):
+    """
+    Import a budget data package into CKAN
+    """
+
+    # Get the datapackage descriptor file
+    response = requests.get(datapackage)
+    descriptor = response.json()
+
+    # If a schema was provided, we validate the datapackage based on the schema
+    if schema is not None:
+        schema_obj = json.load(schema)
+        jsonschema.validate(descriptor, schema_obj)
+
+    # Extract CKAN metadata from the data package
+    data_dict = create_ckan_package_dict(descriptor)
+
+    # Fix urls in resources because paths must be turned into urls
+    # because we don't support file uploads.
+    resources = create_ckan_resource_array(descriptor)
+    for resource in resources['resources']:
+        if 'url' in resource and not (
+                resource['url'].startswith('http://') or
+                resource['url'].startswith('https://')):
+            resource['url'] = urlparse.urljoin(datapackage, resource['url'])
+
+    # Add the data package descriptor file as a resource
+    resources['resources'].append({
+        'name': 'Data package',
+        'description': 'The descriptor file for the data package',
+        'url': datapackage})
+
+    # Append the resources to the package. This allows us to create resources
+    # at the same time as we create the package, but this limits us to linking
+    # to resources (hence the fix above) instead of uploading. If we want to
+    # upload, we need to create each resource on its own.
+    data_dict.update(resources)
+
+    # Add budget data package metadata as extras, this requires that the
+    # CKAN instance will have a schema that accepts these extras
+    data_dict.update(create_budget_data_package_extras(descriptor))
+
+    # We're going to make it really hard for people to know what went wrong
+    # if something fails by only checking status code and raise a generic error
+    if submit_to_ckan(host, apikey, data_dict) != 200:
+        raise IOError('Unable to submit budget data package to CKAN')
+
+if __name__ == '__main__':
+    bdp2ckan()
